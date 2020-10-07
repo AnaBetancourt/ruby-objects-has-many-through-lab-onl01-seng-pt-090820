d2901651b43775700cdaaa93614c2c2376a103be
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-07 07:10:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,10 +12,18 @@ class Artist
     @@all
   end
   
+  def new_song(name, genre)
+    Song.new(name, genre)
+    Song.artist = self
+  end
   
+  def songs 
+    Songs.all.select{|song| song.artist == self}
+  end
   
-  
-  
-  
+  def genres 
+    self.songs.each do |song|
+      song.genre
+  end
   
 end
